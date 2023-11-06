0b9b8553e1915197373ad754f696856c6dc51c27
MarinnaPereira <marinna.dos.anjos@gmail.com>
2023-11-06 15:50:25 +0100

    basic code

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ console.log(`User's input is: ${name}`);
 
 // ------------------------
 
+// Function which creates a secret number with 4 unique digits
 const createSecretNumber = () => {
   const possibleDigits = "0123456789";
   let secretNumber = "";
   while (secretNumber.length < 4) {
-    const randomDigit =
-      possibleDigits[Math.floor(Math.random() * possibleDigits.length)];
+    const randomIndex = Math.floor(Math.random() * possibleDigits.length);
+    const randomDigit = possibleDigits[randomIndex];
     if (!secretNumber.includes(randomDigit)) {
       secretNumber += randomDigit;
     }
@@ -25,3 +26,63 @@ const createSecretNumber = () => {
   console.log(secretNumber);
   return secretNumber;
 };
+
+// Function to check the player's input
+function checkInput(secretNumber, input) {
+  let bulls = 0;
+  let cows = 0;
+  for (let i = 0; i < 4; i++) {
+    if (secretNumber[i] === input[i]) {
+      bulls++;
+    } else if (secretNumber.includes(input[i])) {
+      cows++;
+    }
+  }
+  return { bulls, cows };
+}
+
+// Start function
+const start = () => {
+  const secretNumber = createSecretNumber();
+
+  console.log(
+    `Welcome to Bulls and Cows' game, ${name}! Guess our secret number. It has 4 unique digits.`
+  );
+
+  //   let attempts = 0;
+  do {
+    let input = prompt("Try a number: ");
+    if (input.length !== 4 || !/^[0-9]/.test(input)) {
+      console.log(`Invalid guess. Please write a number with 4 unique digits.`);
+      continue;
+    }
+
+    function hasRepeatedChars(input) {
+      for (let i = 0; i < input.length; i++) {
+        const char = input[i];
+        if (input.includes(char, i + 1)) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    if (hasRepeatedChars(input)) {
+      console.log(`You can't repeat any digit. Try again.`);
+      continue;
+    }
+
+    const result = checkInput(secretNumber, input);
+
+    if (result.bulls === 4) {
+      console.log(`Congratulations. You are a winner!!`);
+      break;
+    } else {
+      console.log(
+        `Here is a hint for you: ${result.bulls} bulls and ${result.cows} cows. Keep going...`
+      );
+    }
+  } while (true);
+};
+
+start();
