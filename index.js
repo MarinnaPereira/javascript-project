58d845f60dda71183e1f3b9a68cdca2f4717b749
MarinnaPereira <marinna.dos.anjos@gmail.com>
2023-11-07 13:46:35 +0100

    added chalk

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 console.clear();
 
 // Bulls and Cows
+
 // Get library for user input
-const prompt = require("prompt-sync")({ sigint: true });
+// const prompt = require("prompt-sync")({ sigint: true });
+import promptSync from "prompt-sync";
+const prompt = promptSync();
+
+// Get library for customize terminal colors
+import chalk from "chalk";
 
 // Get player's name
-let playerName = prompt("Enter your name? ");
+let playerName = prompt(
+  chalk.magenta.bgGreenBright.bold("Enter your name?") + " "
+);
 const name = playerName || "Stranger";
 
 // Function which creates a secret number with 4 unique digits
@@ -20,7 +28,7 @@ const createSecretNumber = () => {
       secretNumber += randomDigit;
     }
   }
-  //   console.log(secretNumber);
+  console.log(secretNumber);
 
   return secretNumber;
 };
@@ -60,19 +68,31 @@ function hasRepeatedChars(input) {
 // Start function
 const start = () => {
   console.log(
-    `${greet(
-      name
-    )}! Welcome to Bulls and Cows! 🎉 It's like being a secret agent on a code-cracking mission. The computer has a 4-digit secret number with unique digits. Your goal? Figure it out! You get 🐂 "Bulls" for the right digits in the right spots and 🐄 "Cows" for the right digits in the wrong spots.\n`
+    chalk.cyanBright(
+      `\n${greet(
+        name
+      )}! Welcome to Bulls and Cows! 🎉 It's like being a secret agent on a code-cracking mission. The computer has a 4-digit secret number with unique digits. Your goal? Figure it out! You get 🐂 "Bulls" for the right digits in the right spots and 🐄 "Cows" for the right digits in the wrong spots.\n`
+    )
   );
 
   console.log(
-    "Before we jump right into your mission, lets's set up its level.\nIn the (1) easy mode you'll have no limit of attempts, but in in the (2) hard mode you'll have a maximum of 8 attempts. So... "
+    chalk.cyanBright(
+      "Before we jump right into your mission, lets's set up its level.\n"
+    )
   );
 
-  const gameMode = prompt("Enter your choice (1/2): ");
+  console.log(
+    chalk.cyanBright(
+      "In the (1) easy mode you'll have no limit of attempts, but in in the (2) hard mode you'll have a maximum of 8 attempts. So... \n"
+    )
+  );
+
+  const gameMode = prompt(
+    chalk.magenta.bgGreenBright.bold("Enter your choice (1/2):") + " "
+  );
   const chosenMode = gameMode.toLowerCase() === "1" ? "Easy mode" : "Hard mode";
 
-  console.log(`Nice! ${chosenMode} it is!`);
+  console.log(chalk.rgb(255, 136, 0)(`\nNice! ${chosenMode} it is!\n`));
 
   const maxAttempts = gameMode.toLowerCase() === "1" ? Infinity : 8;
 
@@ -86,27 +106,37 @@ const start = () => {
     let attempts = 0;
 
     console.log(
-      `Ready for Bulls and Cows? 🎮 Great! Now, give us your best shot.`
+      chalk.cyanBright(
+        `Ready for Bulls and Cows? 🎮 Great! Now, give us your best shot.\n`
+      )
     );
 
     while (attempts < maxAttempts) {
       if (chosenMode !== "Easy mode" && attempts > 0) {
-        console.log(`You have more attempts left: ${maxAttempts - attempts}`);
+        console.log(
+          chalk.yellow(`You have ${maxAttempts - attempts} attempts left.\n`)
+        );
       }
-      let input = prompt("Enter a 4-digit number: ");
+      let input = prompt(
+        chalk.magenta.bgGreenBright.bold("Enter a 4-digit number:") + " "
+      );
 
       // wrong length
       if (input.length !== 4) {
         console.log(
-          `Oops! Your entry should be a 4-digit number. Try again, Agent ${name}! 🕵️‍♂️💡`
+          chalk.red(
+            `\n📢 Oops! Your entry should be a 4-digit number. Try again, Agent ${name}! 🕵️‍♂️💡\n`
+          )
         );
         continue;
       }
 
       // no numeric character
-      if (!/^[0-9]/.test(input)) {
+      if (!/^\d+$/.test(input)) {
         console.log(
-          `Watch out, Agent ${name}! Your entry should contain only numeric digits, no secret symbols or letters. Try again! 🕵️‍♂️🔢\n`
+          chalk.red(
+            `\n📢 Watch out! Your entry should contain only numeric digits, no secret symbols or letters. Try again! 🕵️‍♂️🔢\n`
+          )
         );
         continue;
       }
@@ -114,7 +144,9 @@ const start = () => {
       // repeated character
       if (hasRepeatedChars(input)) {
         console.log(
-          `Whoops! Remember, the code should have four unique numbers, Agent ${name}. No repeats allowed. Try again with distinct digits! 🕵️‍♂️🔢\n`
+          chalk.red(
+            `\n📢 Whoops! Remember, the code should have four unique numbers. No repeats allowed. Try again with distinct digits! 🕵️‍♂️🔢\n`
+          )
         );
         continue;
       }
@@ -125,40 +157,52 @@ const start = () => {
 
       if (bulls === 4) {
         console.log(
-          `Congratulations, Agent ${name}! You cracked the secret code in ${attempts} attempts! You're a code-cracking genius 🏆🎉. You've earned your stripes as the ultimate Bulls and Cows champion! 🥳💼🕵️‍♂️`
+          chalk.green.bold(
+            `\nCongratulations, Agent ${name}! You cracked the secret code in ${attempts} attempts! You're a code-cracking genius 🏆🎉. You've earned your stripes as the ultimate Bulls and Cows champion! 🥳💼🕵️‍♂️\n`
+          )
         );
         break;
       }
       if (bulls === 0 && cows === 0) {
         const noBullsNoCowsMessages = [
-          "Keep trying, you'll get it!",
-          "Don't give up, you're close!",
-          "You can do it, just a little more.",
+          "Keep trying, you'll get it!\n",
+          "Don't give up, you're close!\n",
+          "You can do it, just a little more.\n",
         ];
         console.log(
-          noBullsNoCowsMessages[
-            Math.floor(Math.random() * noBullsNoCowsMessages.length)
-          ]
+          chalk.greenBright(
+            noBullsNoCowsMessages[
+              Math.floor(Math.random() * noBullsNoCowsMessages.length)
+            ]
+          )
         );
       } else {
         console.log(
-          `Here is a hint: you got ${bulls} bulls and ${cows} cows. Keep going...`
+          chalk.greenBright(
+            `Here is a hint: you got ${chalk.white(
+              bulls
+            )} bulls and ${chalk.white(cows)} cows. Keep going...`
+          )
         );
       }
       if (attempts === maxAttempts) {
         console.log(
-          `You've reached the maximum number of attempts.💥 The secret code was ${secretNumber}. Better luck next time!`
+          chalk.red.bold(
+            `You've reached the maximum number of attempts.💥 The secret code was ${secretNumber}. Better luck next time!\n`
+          )
         );
       }
     }
     totalAttempts += attempts;
-    console.log(`Total games played: ${totalGames}`);
-    console.log(`Total attempts made: ${totalAttempts}`);
+    console.log(chalk.magentaBright(`Total games played: ${totalGames}`));
+    console.log(chalk.magentaBright(`Total attempts made: ${totalAttempts}`));
 
-    playAgain = prompt("Play again? (yes/no): ").toLowerCase();
+    playAgain = prompt(
+      chalk.magenta.bgGreenBright.bold("Play again? (yes/no):") + " "
+    ).toLowerCase();
   }
 
-  console.log("Thanks for playing Bulls and Cows!");
+  console.log(chalk.cyanBright("Thanks for playing Bulls and Cows!"));
 };
 
 start();
